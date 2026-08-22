6782ae9faadb867939de26a20ecc05f12a85991a
alexanderliberacion-cmd <alexanderliberacion@gmail.com>
2026-08-22 17:07:48 +0200

    Fix render

--- backend/src/main/java/biblioteca/Spring/Boot/BibliotecaSpringBootApplication.java
+++ backend/src/main/java/biblioteca/Spring/Boot/BibliotecaSpringBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BibliotecaSpringBootApplication {
 
-	 void main(String[] args) {
+	 public static void main(String[] args) {
 		SpringApplication.run(BibliotecaSpringBootApplication.class, args);
 	}
 
