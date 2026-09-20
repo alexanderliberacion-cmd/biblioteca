bcd809c702586ee208040b556c5973c74308b44a
alexanderliberacion-cmd <alexanderliberacion@gmail.com>
2026-09-20 20:41:56 +0200

    Fix to ghpages

--- backend/src/main/java/biblioteca/Spring/Boot/Security/SecurityConfiguration.java
+++ backend/src/main/java/biblioteca/Spring/Boot/Security/SecurityConfiguration.java
@@ -81,7 +81,7 @@ public class SecurityConfiguration {
    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
        CorsConfiguration configuration = new CorsConfiguration();
-       configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+       configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://alexanderliberacion-cmd.github.io"));
        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH","DELETE"));
        configuration.setAllowedHeaders (List.of("Content-Type","Authorization"));
        UrlBasedCorsConfigurationSource source  = new  UrlBasedCorsConfigurationSource();
