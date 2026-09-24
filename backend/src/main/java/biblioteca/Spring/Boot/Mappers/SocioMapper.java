5b6d1715b148add685c3b0167679d41861499fc3
alexanderliberacion-cmd <alexanderliberacion@gmail.com>
2026-09-24 13:02:03 +0200

    Fix to ghpages

--- backend/src/main/java/biblioteca/Spring/Boot/Mappers/SocioMapper.java
+++ backend/src/main/java/biblioteca/Spring/Boot/Mappers/SocioMapper.java
@@ -3,9 +3,12 @@ package biblioteca.Spring.Boot.Mappers;
 import biblioteca.Spring.Boot.DTO.SocioDTO;
 import biblioteca.Spring.Boot.Entities.SocioEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SocioMapper {
+
    SocioDTO socioADto(SocioEntity socio);
+   @Mapping(target = "idSocio", ignore = true)
     SocioEntity dtoASocio(SocioDTO socioDTO);
 }
